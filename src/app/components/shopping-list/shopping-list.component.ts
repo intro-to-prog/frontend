88746d463ef738273d95126893ca14a0a28d0eb1
Jeffry Gonzalez <jeff_gonzalez@progressive.com>
2021-09-01 12:33:10 -0400

    before lunch wednesday

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
 import { ShoppingListItemModel } from 'src/app/models';
+import { AppState, selectShoppingListItemModel } from 'src/app/reducers';
 
 @Component({
   selector: 'app-shopping-list',
@@ -8,13 +11,13 @@ import { ShoppingListItemModel } from 'src/app/models';
 })
 export class ShoppingListComponent implements OnInit {
 
-  items: ShoppingListItemModel[] = [
-    { id: '1', description: 'Shampoo' },
-    { id: '99', description: 'Conditioner' }
-  ];
-  constructor() { }
+  items$!: Observable<ShoppingListItemModel[]>;
+  constructor(private store: Store<AppState>) {
+
+  }
 
   ngOnInit(): void {
+    this.items$ = this.store.select(selectShoppingListItemModel);
   }
 
   markPurchased(item: ShoppingListItemModel) {
